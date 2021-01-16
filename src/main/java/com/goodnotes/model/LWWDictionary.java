4b7223810b4b3c17400683d72b88385c4864ff89
Asif Hussain <asif.h1990@gmail.com>
2021-01-16 13:03:40 +0530

    Added: add() method to add elements to dictionary, size() method to get size, lookup() method to lookup element in dictionary, containsKey() to check if key is present in dictionary

---
+++ src/main/java/com/goodnotes/model/LWWDictionary.java
@@ -0,0 +1,51 @@
+package com.goodnotes.model;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LWWDictionary<K, V> {
+
+    Map<K, TimestampedValue> addMap;
+    Map<K, LocalDateTime> removeMap;
+
+    public LWWDictionary() {
+        addMap = new HashMap<>();
+        removeMap = new HashMap<>();
+    }
+
+    public boolean containsKey(K key) {
+        return addMap.containsKey(key) && (!removeMap.containsKey(key) || removeMap.get(key).isBefore(addMap.get(key).timestamp));
+    }
+
+    public void add(K key, V value) {
+        add(key, value, LocalDateTime.now());
+    }
+
+    public void add(K key, V value, LocalDateTime timestamp) {
+        addMap.put(key, new TimestampedValue(value, timestamp));
+    }
+
+    public int size() {
+        return (int) addMap.entrySet().stream().filter(entry -> !removeMap.containsKey(entry.getKey()) || entry.getValue().timestamp.isAfter(removeMap.get(entry.getKey()))).count();
+    }
+
+    public V lookup(K key){
+        return containsKey(key) ? (V) addMap.get(key).value : null;
+    }
+
+    class TimestampedValue<V> {
+        LocalDateTime timestamp;
+        V value;
+
+        public TimestampedValue(V value) {
+            this.value = value;
+            this.timestamp = LocalDateTime.now();
+        }
+
+        public TimestampedValue(V value, LocalDateTime timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+}
--- src/test/java/com/goodnotes/model/LWWDictionaryTest.java
+++ src/test/java/com/goodnotes/model/LWWDictionaryTest.java
@@ -1,10 +1,17 @@
 package com.goodnotes.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 public class LWWDictionaryTest {
     @Test
     public void shouldAddElementToDictionary() {
+        LWWDictionary dictionary = new LWWDictionary();
+        dictionary.add(1, 5, LocalDateTime.now());
 
+        Assertions.assertEquals(1, dictionary.size());
+        Assertions.assertEquals(5, dictionary.lookup(1));
     }
 }
