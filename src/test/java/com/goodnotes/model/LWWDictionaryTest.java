a1cf4398aaff3abf07971f296ade8f35edbf72eb
Asif Hussain <asif.h1990@gmail.com>
2021-01-16 13:09:01 +0530

    test to lookup key and return corresponding value

--- src/test/java/com/goodnotes/model/LWWDictionaryTest.java
+++ src/test/java/com/goodnotes/model/LWWDictionaryTest.java
@@ -1,17 +1,32 @@
 package com.goodnotes.model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 public class LWWDictionaryTest {
+    private LWWDictionary<Integer, Integer> dictionary;
+    @BeforeEach
+    public void initialize(){
+        dictionary = new LWWDictionary();
+    }
+
     @Test
     public void shouldAddElementToDictionary() {
-        LWWDictionary dictionary = new LWWDictionary();
         dictionary.add(1, 5, LocalDateTime.now());
 
         Assertions.assertEquals(1, dictionary.size());
         Assertions.assertEquals(5, dictionary.lookup(1));
     }
+
+    @Test
+    public void shouldLookupKeyAndReturnValue(){
+        dictionary.add(1, 20, LocalDateTime.now());
+
+        Assertions.assertEquals(dictionary.lookup(1), 20);
+    }
+
+
 }
